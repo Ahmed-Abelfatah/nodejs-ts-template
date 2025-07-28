5e93490aeb2d47f6f9dc23dc303433d25730e627
Ahmed-Adbelfatah <ahmed.m.web.dev@gmail.com>
2025-07-29 00:54:14 +0400

    fix: remove bin dir and initiate git

--- bin/cli.js
+++ bin/cli.js
@@ -13,9 +13,12 @@ const runCommand = (command) => {
 const repoName = process.argv[2];
 const checkoutCommand = `git clone --depth 1 https://github.com/Ahmed-Abelfatah/nodejs-ts-template ${repoName}`;
 const installDependancies = `cd ${repoName} && yarn install`;
+const cleanUpAndInitiateGit = `rm -rf .git bin && git init`
 console.log(`🏗️  👷👷 Cloning the repository with name ${repoName} 👷👷 🏗️`);
 const checkout = runCommand(checkoutCommand);
 if (!checkout) process.exit(-1);
+const cleanUpAndInit = runCommand(cleanUpAndInitiateGit)
+if(!cleanUpAndInit) process.exit(-1)
 console.log(`🔨🔨🔨 Installing the dependancies 🔨🔨🔨`);
 const installPackages = runCommand(installDependancies);
 if (!installPackages) process.exit(-1);
